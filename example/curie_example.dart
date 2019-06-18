e71601a63ea9da7cd67e34b7d004b9088fb67cc0
huanghaiyang <haiyang.huang@forceclouds.com>
2019-06-18 17:51:43 +0800

    remove samples

--- example/curie_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:curie/curie.dart';
-
-main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/curie.dart
+++ lib/curie.dart
@@ -3,6 +3,3 @@
 /// More dartdocs go here.
 library curie;
 
-export 'src/curie_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
