110b57b5c82663589614292375dc9989b1d7f83e
huanghaiyang <haiyang.huang@forceclouds.com>
2019-06-18 11:46:57 +0800

    initial

---
+++ example/curie_example.dart
@@ -0,0 +1,6 @@
+import 'package:curie/curie.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/curie.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library curie;
+
+export 'src/curie_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
