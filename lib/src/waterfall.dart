e48141483ed0d3a08c6916a62db679d6eb1cef6b
huanghaiyang <huanhai24@sina.com.cn>
2019-12-23 10:12:37 +0800

    update

--- lib/src/waterfall.dart
+++ lib/src/waterfall.dart
@@ -1,6 +1,10 @@
 Future<dynamic> waterfall(List<Function> functions) async {
   dynamic preResult;
+  List<int> list = List();
   for (int i = 0; i < functions.length; i++) {
+    list.add(i);
+  }
+  await for (int i in Stream.fromIterable(list)) {
     Function function = functions[i];
     preResult = await Function.apply(function, [preResult]);
   }
